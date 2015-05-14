55a012053108a4adc0c145b6bc095cc89c2e2e59
Zhang Fan <827094173@qq.com>
2015-05-14 13:35:38 +0800

    在实验室配制完后，能在sae上部署

--- src/zhangfan/weixin/serviceimpl/StuRoleServiceImpl.java
+++ src/zhangfan/weixin/serviceimpl/StuRoleServiceImpl.java
@@ -1,7 +1,18 @@
 package zhangfan.weixin.serviceimpl;
 
+import zhangfan.weixin.dao.StuRoleDao;
 import zhangfan.weixin.service.StuRoleService;
 
 public class StuRoleServiceImpl implements StuRoleService {
 
+	private StuRoleDao StuRoleDao;
+
+	public StuRoleDao getStuRoleDao() {
+		return StuRoleDao;
+	}
+
+	public void setStuRoleDao(StuRoleDao stuRoleDao) {
+		StuRoleDao = stuRoleDao;
+	}
+	
 }
